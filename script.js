3c54c158082e24ddd87e9082310136ee3fa435a1
Sandrine Poissonnet <petitspoissons@icloud.com>
2021-06-27 19:12:39 -0700

    slider edits

--- script.js
+++ script.js
@@ -1,28 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
-  // initialize carousel
-  var carousel = document.querySelectorAll('.carousel');
-  M.Carousel.init(carousel, {
-    fullWidth: true,
-    indicators: true,
-    duration: 300,
-    padding: 10,
+  // initialize sidenav
+  var sidenavEl = document.querySelectorAll('.sidenav');
+  M.Sidenav.init(sidenavEl);
+
+  // initialize slider
+  var sliderEl = document.querySelectorAll('.slider');
+  M.Slider.init(sliderEl, {
+    indicators: false,
+    height: 500,
   });
 
-  // autoplay custom function
-  const indicatorItems = document.querySelectorAll('.carousel .indicator-item');
-  const slideTime = 5000;
-  const activeClass = 'active';
-
-  setInterval(() => {
-    indicatorItems.forEach((el) => {
-      if (el.classList.contains(activeClass)) {
-        let siblingEl = el.nextElementSibling;
-        if (siblingEl == null) {
-          indicatorItems[0].click();
-        } else {
-          siblingEl.click();
-        }
-      }
-    });
-  }, slideTime);
+  // initialize parallax
+  var parallaxEl = document.querySelectorAll('.parallax');
+  M.Parallax.init(parallaxEl);
 });
